9eff0ded1392cf02611770a090267faeaea37c95
Thiyagarajan <thiyagaraja04@yahoo.com>
2021-07-28 15:26:37 +0530

    Changes for service integration and toastr notification integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EventService } from './service/event.service';
+import { ToastrService } from './service/toastr.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventService,ToastrService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EventService } from '../service/event.service';
+import { ToastrService } from '../service/toastr.service';
 
 @Component({
   selector: 'app-event-list',
@@ -7,95 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private eventService:EventService,private toastr:ToastrService) { }
+  @Input() eventlist:any
   ngOnInit(): void {
+    this.eventlist = this.eventService.getEvents();
   }
-  eventlist = [{
-    "id": 1,
-    "name" : "Leh-Ladak Bike trip",
-    "date" : "8-August-2021",
-    "time" : "8.00 am",
-    "price" : 25000,
-    "location": {
-      "address": "1 Leh Market",
-      "city" : "Leh",
-      "country": "India",
-      "province": "Ladakh"
-    
-    },
-    "eventType": "Adventure",
-    "difficulty": "Moderate"
-  },{
-    "id": 2,
-    "name" : "Andaman Couple trip",
-    "date" : "3-March-2021",
-    "time" : "8.00 am",
-    "price" : 25000,
-    "location": {
-      "address": "1 South Andaman Market",
-      "city" : "Andaman and Nicobar",
-      "country": "India",
-      "province": "South Andaman"
-    },
-    "eventType": "Leisure",
-    "difficulty": "Easy"
-  },{
-    "id": 3,
-    "name" : "TNPSC online class",
-    "date" : "11-11-2021",
-    "time" : "7.00 pm",
-    "price" : 0,
-    "onlineUrl" : "https://tnpscexams.net",
-    "eventType": "Educational"
-  },{
-    "id": 4,
-    "name" : "TNPSC online class",
-    "date" : "11-11-2021",
-    "time" : "7.00 pm",
-    "price" : 0,
-    "onlineUrl" : "https://tnpscexams.net",
-    "eventType": "Educational"
-  },{
-    "id": 5,
-    "name" : "TNPSC online class",
-    "date" : "11-11-2021",
-    "time" : "7.00 pm",
-    "price" : 0,
-    "onlineUrl" : "https://tnpscexams.net",
-    "eventType": "Educational"
-  },{
-    "id": 6,
-    "name" : "TNPSC online class",
-    "date" : "11-11-2021",
-    "time" : "7.00 pm",
-    "price" : 0,
-    "onlineUrl" : "https://tnpscexams.net",
-    "eventType": "Educational"
-  },{
-    "id": 7,
-    "name" : "TNPSC online class",
-    "date" : "11-11-2021",
-    "time" : "7.00 pm",
-    "price" : 0,
-    "onlineUrl" : "https://tnpscexams.net",
-    "eventType": "Educational"
-  },{
-    "id": 8,
-    "name" : "Ortho Doctor Visit",
-    "date" : "3-March-2021",
-    "time" : "8.00 am",
-    "price" : 25000,
-    "location": {
-      "address": "1 South Andaman Market",
-      "city" : "Andaman and Nicobar",
-      "country": "India",
-      "province": "South Andaman"
-    },
-    "eventType": "Checkup",
-    "difficulty": "NA"
+  
+  handleEventClickEvent(eventName:string){
+    this.toastr.success(eventName)
   }
-]
 
 /*
 
